6c7c3086c2a5ffb859695d9a1fcf29e50bfbee3e
gjcooper99 <gjcooper99@gmail.com>
2021-02-08 12:15:56 -0800

    Added Search Functionality to Tables

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    var rows = table.getElementsByTagName("tr");
+    for (i = 1; i < rows.length-1; i++) {
+      var cells = rows[i].getElementsByTagName("td");
+      var j;
+      var rowContainsFilter = false;
+      for (j = 0; j < cells.length; j++) {
+        if (cells[j]) {
+          if (cells[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            rowContainsFilter = true;
+            continue;
+          }
+        }
+      }
+      if (! rowContainsFilter) {
+        rows[i].style.display = "none";
+      } else {
+        rows[i].style.display = "";
+      }
+    }
+  }
