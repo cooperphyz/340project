9a34f008725f15fac5c5b97cd044431af9136e28
gjcooper99 <gjcooper99@gmail.com>
2021-02-17 20:08:32 -0800

    Added Demo Profile Pages for Student/Teacher

--- JS/script.js
+++ JS/script.js
@@ -23,3 +23,30 @@ function myFunction() {
       }
     }
   }
+
+ function myProfileFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    var rows = table.getElementsByTagName("tr");
+    for (i = 2; i < rows.length-1; i++) {
+      var cells = rows[i].getElementsByTagName("td");
+      var j;
+      var rowContainsFilter = false;
+      for (j = 0; j < cells.length; j++) {
+        if (cells[j]) {
+          if (cells[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            rowContainsFilter = true;
+            continue;
+          }
+        }
+      }
+      if (! rowContainsFilter) {
+        rows[i].style.display = "none";
+      } else {
+        rows[i].style.display = "";
+      }
+    }
+  }
+
